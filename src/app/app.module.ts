d4b0a61d41484dd56540bef43aa6072659aecfdd
mromerod <maramanss99@gmail.com>
2023-04-12 19:37:30 +0200

    estado inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PostComponent } from './post/post.component';
+import { HttpClientModule} from '@angular/common/http';
+import { LoginAuthComponent } from './login-auth/login-auth.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    LoginAuthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
